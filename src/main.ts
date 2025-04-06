46d8539213f4ad5fd07b773d46a91f14001a2afb
mapaille <mpaille@live.ca>
2025-04-06 16:03:47 -0400

    Add app id.

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ import router from './router'
 import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 
 const appInsights = new ApplicationInsights({ config: {
-  connectionString: 'InstrumentationKey=5607b793-6c98-4ea1-ade0-cc3f4743bd14;IngestionEndpoint=https://canadaeast-0.in.applicationinsights.azure.com/;LiveEndpoint=https://canadaeast.livediagnostics.monitor.azure.com/;ApplicationId=f3769436-9a29-4335-ae8e-41a7d1971fd8'
+  connectionString: 'InstrumentationKey=5607b793-6c98-4ea1-ade0-cc3f4743bd14;IngestionEndpoint=https://canadaeast-0.in.applicationinsights.azure.com/;LiveEndpoint=https://canadaeast.livediagnostics.monitor.azure.com/;ApplicationId=f3769436-9a29-4335-ae8e-41a7d1971fd8',
+  appId: 'tools'
 } });
 appInsights.loadAppInsights();
 appInsights.trackPageView();
