15288a8186e98ae17660f6235c85409f4d72c31c
mapaille <mpaille@live.ca>
2025-05-04 09:02:40 -0400

    Cleanup.

--- src/router.ts
+++ src/router.ts
@@ -11,7 +11,7 @@ const routes = [
     { path: '/guid', component: GuidView },
     { path: '/sql', component: SqlView }
 ];
-  
+
 const router = createRouter({
     history: createWebHistory(),
     routes
